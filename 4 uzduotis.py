5ff3494e0ec962e26ed256fa9595f8f205120e1f
Karolina Grin <karolina.griniute@codeacademylt.omicrosoft.com>
2022-11-14 21:47:04 +0200

    4uzduotis

---
+++ 4 uzduotis.py
@@ -0,0 +1,36 @@
+# Sukurkite klasę "Movie", kuri gebės sukurti objektus 3 savybėm ir 1 metodu. 
+# Naudojant šią klasę sukurkite bent du skirtingus objektus.
+
+# Savybės:
+# title: string
+# director: string
+# budget: number
+
+# Metodas: 
+# was_expensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins True, kitu atveju False.
+
+
+class Movie:
+    def __init__(self, title = "Python", director = "Karolina", budget = 500):
+        self.title = title
+        self.director = director
+        self.budget = budget
+
+
+movie1 = Movie("HTML", "Lukas", 400)
+print(movie1.title, movie1.director)
+
+
+
+
+#movie1 = Movie("Python", "Karolina G.", 100)
+#movie2 = Movie("JS", "Lukas", 10)
+#movie3 = Movie("HTML", "Jonas", 30)
+
+#print(movie1.title)
+
+#movie2.budget = 500
+#print(movie2.budget)
+
+
+
